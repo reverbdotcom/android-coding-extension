a1bf33c43b6794173e74a74b8a724b814b5adbd0
Pete Ross <peteross@reverb.com>
2025-05-29 09:13:53 -0500

    Make the sort an input to the query

--- app/src/main/java/com/reverb/android/onsite/data/repository/RecentListingsRepository.kt
+++ app/src/main/java/com/reverb/android/onsite/data/repository/RecentListingsRepository.kt
@@ -2,6 +2,7 @@ package com.reverb.android.onsite.data.repository
 
 import com.apollographql.apollo.ApolloClient
 import com.reverb.android.onsite.Android_Onsite_RecentListingsQuery
+import com.reverb.android.onsite.type.Reverb_search_ListingsSearchRequest_Sort
 import org.koin.core.annotation.Single
 
 @Single
@@ -11,7 +12,7 @@ class RecentListingsRepository(
 
   suspend fun fetchRecentListings(): List<Android_Onsite_RecentListingsQuery.Data.ListingsSearch.Listing>? {
     return apolloClient
-      .query(Android_Onsite_RecentListingsQuery())
+      .query(Android_Onsite_RecentListingsQuery(Reverb_search_ListingsSearchRequest_Sort.CREATED_AT_DESC))
       .execute()
       .data
       ?.listingsSearch
